de17aa3436a1df406a8c90a9f18bf607376d1b03
Sam Corcos <sam@andaril.com>
2014-05-21 15:17:11 -0700

    setting up templates

---
+++ collections/unit_value.js
@@ -0,0 +1,51 @@
+unitValue = {
+    'aircraft': {
+        'attack': 1,
+        'defense': 1,
+        'cost': 10
+    },
+    'supplied': {
+        'infantry': {
+            'attack': 2,
+            'defense': 3,
+            'cost': 5
+        },
+        'armor': {
+            'attack': 4,
+            'defense': 2,
+            'cost': 5
+        },
+        'lightFleet': {
+            'attack': 2,
+            'defense': 2,
+            'cost': 5
+        },
+        'heavyFleet': {
+            'attack': 4,
+            'defense': 2,
+            'cost': 5
+        }
+    },
+    'unsupplied': {
+        'infantry': {
+            'attack': 1,
+            'defense': 1,
+            'cost': 5
+        },
+        'armor': {
+            'attack': 1,
+            'defense': 1,
+            'cost': 5
+        },
+        'lightFleet': {
+            'attack': 1,
+            'defense': 1,
+            'cost': 5
+        },
+        'heavyFleet': {
+            'attack': 1,
+            'defense': 1,
+            'cost': 5
+        }
+    }
+}
