30d9200b2405bc08ace20a0a765205ce2f0470d7
Sam Corcos <sam@andaril.com>
2014-05-21 15:33:28 -0700

    finalizing templates

---
+++ client/views/battle.js
@@ -0,0 +1,24 @@
+
+
+// We have armies:
+
+// 	attacking
+// 	or defending
+
+
+// Armies are comprised of:
+
+// 	airplanes
+// 	sea combat
+// 		heavy
+// 		light
+// 		unsupplied heavy
+// 		unsupplied light
+// 	land combat
+// 		infantry
+// 		tank
+// 		unsupplied infantry
+// 		unsupplied tank
+
+
+
